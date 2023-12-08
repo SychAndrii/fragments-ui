7872f866407bf26ded27429a4dee8048749e17ee
SychAndrii <asych@myseneca.ca>
2023-12-07 21:31:28 -0500

    Done with testing

--- src/pages/fragment/fragmentLoader.ts
+++ src/pages/fragment/fragmentLoader.ts
@@ -2,29 +2,33 @@ import { LoaderFunctionArgs, Params } from "react-router-dom";
 import { getUser } from "../../auth";
 
 export default async function fragmentLoader(args: LoaderFunctionArgs<Params>) {
-    const { params } = args;
-    const user = await getUser();
-    
-    const res = await fetch(`${import.meta.env.VITE_API_URL}/fragments/${params.id}`, {
-      headers: {
-        ...user?.authorizationHeaders()
-      }
-    });
-    const type = res.headers.get("Content-Type");
-    console.log(type);
-    
-    
-    const content = !type?.startsWith('text') ? await res.blob() : await res.text();
-    console.log(content);
-    
+  const { params } = args;
+  const user = await getUser();
 
-    const location = `${import.meta.env.VITE_API_URL}/fragments/${params.id}`;
-  
-    return {
-      content,
-      id: params.id,
-      size: +res.headers.get("Content-Length")!,
-      type,
-      location,
-    };
-  }
+  const res = await fetch(
+    `${import.meta.env.VITE_API_URL}/fragments/${params.id}`,
+    {
+      headers: {
+        ...user?.authorizationHeaders(),
+      },
+    }
+  );
+  const type = res.headers.get("Content-Type");
+  console.log(type);
+
+  const content =
+    !type?.startsWith("text") && !type?.startsWith("application/json")
+      ? await res.blob()
+      : await res.text();
+  console.log(content);
+
+  const location = `${import.meta.env.VITE_API_URL}/fragments/${params.id}`;
+
+  return {
+    content,
+    id: params.id,
+    size: +res.headers.get("Content-Length")!,
+    type,
+    location,
+  };
+}
